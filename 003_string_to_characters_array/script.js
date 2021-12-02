d18e4d26c14dab3774244deed0eb88b6f99bc105
Bennie Draaijer <benniedraaijer@icloud.com>
2021-12-02 19:49:15 +0100

    003 | String To Characters Array

---
+++ 003_string_to_characters_array/script.js
@@ -0,0 +1,10 @@
+const string = 'Come To The Code';
+const array = [...string];
+
+console.log(array);
+
+// Result:
+//     'C', 'o', 'm', 'e',
+//     ' ', 'T', 'o', ' ',
+//     'T', 'h', 'e', ' ',
+//     'C', 'o', 'd', 'e'
